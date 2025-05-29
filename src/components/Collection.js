723f034422ee36da36572891460413c0faad9573
Amy <chiangyenhui@gmail.com>
2025-05-29 10:32:12 +0800

    add searchBar and  update Collection

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,4 +1,18 @@
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import { SEARCH_KEY, BASE_URL, TOKEN_KEY } from "../constant";
+
 const Collection = (props) => {
-  return <div>Collection</div>;
+  const [searchOption, setSearchOption] = useState({
+    type: SEARCH_KEY.all,
+    keyword: "",
+  });
+
+  const handleSearch = (option) => setSearchOption(option);
+  return (
+    <div className="home">
+      <SearchBar handleSearch={handleSearch} />
+    </div>
+  );
 };
 export default Collection;
